518a726b5ed7c7799ee486a3cb852de6dccfbac9
therenatus <therinatus@gmail.com>
2023-06-02 21:59:20 +0600

    ddd

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 const service = new PostService();
 
 router.get('/', service.getAll );
-router.post('/',CreatePostDto, InputValidationMiddleware, service.create);
+router.post('/',CreatePostDto(), InputValidationMiddleware, service.create);
 router.get('/:id', service.getOne);
 router.put('/:id', service.update);
 router.delete('/:id', service.delete);
