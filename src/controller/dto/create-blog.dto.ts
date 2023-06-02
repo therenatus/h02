e7f130b7a95c31337ad765843c1cf2e554c1b9a8
therenatus <therinatus@gmail.com>
2023-06-02 16:39:30 +0600

    sdfsd

--- src/controller/dto/create-blog.dto.ts
+++ src/controller/dto/create-blog.dto.ts
@@ -2,7 +2,7 @@ import {body} from "express-validator";
 
 export const CreateBlogDto = () => {
   return [
-    body('title').trim().notEmpty().isString().isLength({min: 1, max: 15}),
+    body('name').trim().notEmpty().isString().isLength({min: 1, max: 15}),
     body('description').trim().notEmpty().isLength({min: 1, max: 500}),
     body('websiteUrl').isLength({min: 1, max: 100}).isURL()
   ];
